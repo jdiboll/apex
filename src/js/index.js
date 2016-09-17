13751718ecc95f514f8e846a4eab8f66ba5e438c
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-09-17 10:18:34 -0400

    setting up spine of js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 // Javascript Entry Point
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Input from './input';
+
+
+
+
+ReactDOM.render (
+	( <Input />
+		), document.querySelector('.app'));
---
+++ src/js/input.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { ajax } from 'jquery';
+
+
+
+export default class Input extends Component {
+
+
+
+
+
+
+
+
+
+	render() {
+		return (
+			<div className="appointment-wrapper">
+
+
+
+
+			</div>
+
+			);
+	};
+};
